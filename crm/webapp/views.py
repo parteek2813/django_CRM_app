d84c3917e0ed64ec6bab8f8e7605bb9a13b781d9
parteek kumar <digitalparteek@gmail.com>
2023-12-08 22:33:17 +0530

    register & login functionality done

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from .forms import CreateUserForm, LoginForm
 
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
+
+
 def home(request):
     return render(request, 'webapp/index.html')
 
@@ -21,3 +25,31 @@ def register(request):
 
     context = {'form': form}  
     return render(request, 'webapp/register.html', context=context)      
+
+
+# LOGIN A USER
+def my_login(request):
+    
+    form = LoginForm()
+
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+
+    if form.is_valid():
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+
+            auth.login(request, user)
+
+            # return redirect('')
+        
+    context = {'form': form}
+
+    return render(request, 'webapp/my-login.html', context=context)
+
+
